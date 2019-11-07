fa608dea0799110fa85d3a6cfc7660314eb48e03
SamaraBeatriz <43576328+SamaraBeatriz@users.noreply.github.com>
2019-11-07 11:06:37 -0300

    change1

---
+++ controllers/notFoundController.php
@@ -0,0 +1,12 @@
+<?php
+    class notFoundController extends controller {
+        public function __construct() {
+            parent::__construct();
+        }
+        
+        public function index() {
+            $dados = array();
+            
+            $this->loadview('404', $dados);
+        }
+    }
---
+++ models/brand.php
@@ -0,0 +1,3 @@
+<?php
+    class brand extends model{}
+?>
