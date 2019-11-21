1b8ead6ae9a39d8627384090ee9c544d29637c31
SamaraBeatriz <43576328+SamaraBeatriz@users.noreply.github.com>
2019-11-21 11:25:47 -0300

    view-product_item

---
+++ views/product_item.php
@@ -0,0 +1,26 @@
+<div class="product_item">
+    <a href="">
+        <div class="product_tags">
+            <div class="product_tag product_tag_red">SALE</div>
+            <div class="product_tag product_tag_green">BESTSELLER</div>
+            <div class="product_tag product_tag_blue">NEW</div>
+        </div>
+        <div class="product_image">
+            <img src="Media/Products/1.jpg" width="100%" />
+        </div>
+        <div class="product_name">
+            Monitor X
+        </div>
+        <div class="product_brand">
+            Marca Y
+        </div>
+        <div class="product_price_from">
+            R$ xxx
+        </div>
+        <div class="product_price">
+            R$ yyy
+        </div>
+        
+        <div style="clear:both;"></div>
+    </a>
+</div>
